c7a5a0fbf6205aa9ee686dd150924dff01203f47
shehabshalan <shehab.shalan@hotmail.com>
2022-11-08 23:03:50 +0100

    problem: 412

--- easy/412-fizzBuzz.js
+++ easy/412-fizzBuzz.js
@@ -19,3 +19,40 @@ var fizzBuzz = function (n) {
 
   return result;
 };
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+let list1 = [1, 2, 4],
+  list2 = [1, 3, 4];
+let list11 = [],
+  list22 = [];
+let list111 = [],
+  list222 = [0];
+
+function mergeTwoLinkedLists(list1, list2) {
+  let head = { val: -1, next: null };
+  let current = head;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  if (list1) {
+    current.next = list1;
+  } else {
+    current.next = list2;
+  }
+
+  return head.next;
+}
